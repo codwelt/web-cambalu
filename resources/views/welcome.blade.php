a552f8666dc18aa8dce75c776e61512d1f90fef9
FuriosoJack <iam@furiosojack.com>
2019-08-26 10:51:53 -0500

    Se añade foot al card para indicar de que materia es

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
                         <div class="card-body">
                             <h5 class="card-title">{{$trabajo->titulo}}</h5>
                             <p class="card-text">{{$trabajo->descripcion}}</p>
+                            <div class="card-footer">
+                                <a href="#" class="badge badge-warning">{{$trabajo->materia->nombre}}</a>
+                            </div>
                         </div>
                     </div>
                 </div>
