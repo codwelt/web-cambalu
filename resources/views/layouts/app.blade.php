4f40eaf2cf5118d627fc8d901c2911fdc3dbad80
FuriosoJack <iam@furiosojack.com>
2019-08-26 14:36:29 -0500

    Se cambia color del bav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color: #ffc107; !important">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{asset('img/logo2.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
