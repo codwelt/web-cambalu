84d0aeb46ab4e88d718e3d5452df49ba10381e94
FuriosoJack <iam@furiosojack.com>
2019-08-26 10:57:51 -0500

    Se hace refectorizacion de vista principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,19 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row justify-content-center">
+        <div class="card-deck">
             @foreach($trabajos as $trabajo)
-                <div class="col-md-8">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$trabajo->titulo}}</h5>
-                            <p class="card-text">{{$trabajo->descripcion}}</p>
-                            <div class="card-footer">
-                                <a href="#" class="badge badge-warning">{{$trabajo->materia->nombre}}</a>
-                            </div>
-                        </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$trabajo->titulo}}</h5>
+                        <p class="card-text">{{$trabajo->descripcion}}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="badge badge-warning">{{$trabajo->materia->nombre}}</a>
                     </div>
                 </div>
             @endforeach
+
         </div>
     </div>
 @endsection
