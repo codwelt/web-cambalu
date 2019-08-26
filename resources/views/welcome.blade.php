8fdcd53f52f38836fdf31de49c453100207af6fa
FuriosoJack <iam@furiosojack.com>
2019-08-26 10:59:14 -0500

    Se hace refectorizacion de vista principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container">
         <div class="card-deck">
             @foreach($trabajos as $trabajo)
                 <div class="card">
