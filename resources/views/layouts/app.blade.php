2048f960c1d720f146e008a73966b34ea8f92c54
FuriosoJack <iam@furiosojack.com>
2019-08-27 17:03:36 -0500

    Se hace correcion de foot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,14 @@
     <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.js"></script>
 
 
     <!-- Fonts -->
@@ -24,8 +28,10 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.css">
     <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
@@ -39,7 +45,11 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146475064-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'UA-146475064-1');
@@ -52,50 +62,59 @@
             background-color: #090c14;
             color: #878c94;
         }
-        .footer .title{
+
+        .footer .title {
             text-align: left;
-            color:#fff;
-            font-size:25px;
+            color: #fff;
+            font-size: 25px;
         }
-        .footer .social-icon{
-            padding:0px;
-            margin:0px;
+
+        .footer .social-icon {
+            padding: 0px;
+            margin: 0px;
         }
-        .footer .social-icon a{
-            display:inline-block;
-            color:#fff;
-            font-size:25px;
-            padding:5px;
+
+        .footer .social-icon a {
+            display: inline-block;
+            color: #fff;
+            font-size: 25px;
+            padding: 5px;
         }
-        .footer .acount-icon a{
-            display:block;
-            color:#fff;
-            font-size:18px;
-            padding:5px;
-            text-decoration:none;
+
+        .footer .acount-icon a {
+            display: block;
+            color: #fff;
+            font-size: 18px;
+            padding: 5px;
+            text-decoration: none;
         }
-        .footer .acount-icon .fa{
-            margin-right:25px;
+
+        .footer .acount-icon .fa {
+            margin-right: 25px;
         }
+
         .footer .category a {
             text-decoration: none;
             color: #fff;
             display: inline-block;
             padding: 5px 20px;
             margin: 1px;
-            border-radius:4px;
+            border-radius: 4px;
             margin-top: 6px;
             background-color: black;
             border: solid 1px #fff;
         }
-        .footer .payment{
-            margin:0px;
-            padding:0px;
-            list-style-type:none
+
+        .footer .payment {
+            margin: 0px;
+            padding: 0px;
+            list-style-type: none
         }
-        .footer .payment li{
-            list-style-type:none
+
+        .footer .payment li {
+            list-style-type: none
         }
+
         .footer .payment li a {
             text-decoration: none;
             display: inline-block;
@@ -108,103 +127,113 @@
 
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color: #ffc107; !important">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('garden') }}">
-                    <img src="{{asset('img/logo2.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color: #ffc107; !important">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('garden') }}">
+                <img src="{{asset('img/logo2.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    @auth
-                    <!-- Left Side Of Navbar -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            @auth
+                <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('trabajo.create') }}">{{ __('Crear Trabajo') }}</a>
                         </li>
                     </ul>
-                    @endauth
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+            @endauth
+            <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register') && 1 == 0)
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register') && 1 == 0)
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4 bg-light">
-            @yield('content')
-        </main>
-        <script>
-            $('#flash-overlay-modal').modal();
-        </script>
-    </div>
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-3">
-                    <h4 class="title">Acerca de</h4>
-                    <p>Cambalú es un directorio de trabajos universitarios de codigo abierto bajo la licencia <a href="https://www.gnu.org/licenses/agpl-3.0.html" target="_blank"> AGPLv3.</a></p>
-                    <ul class="social-icon">
-                        <a href="https://www.facebook.com/cambaluuu" class="social"><i class="fab fa-facebook-square"></i></a>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h4 class="title">Mi cuenta</h4>
-                    <span class="acount-icon">
-            <a href="{{route('login')}}"><i class="fa fa-user" aria-hidden="true"></i>Perfil</a>
+    <main class="py-4 bg-light">
+        @yield('content')
+    </main>
+    <script>
+        $('#flash-overlay-modal').modal();
+    </script>
+</div>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <h4 class="title">Acerca de</h4>
+                <p>Cambalú es un directorio de trabajos universitarios de codigo abierto bajo la licencia <a
+                            href="https://www.gnu.org/licenses/agpl-3.0.html" target="_blank"> AGPLv3.</a></p>
+                <ul class="social-icon">
+                    <a href="https://www.facebook.com/cambaluuu" class="social"><i
+                                class="fab fa-facebook-square"></i></a>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h4 class="title">Mi cuenta</h4>
+                <span class="acount-icon">
+                <a href="{{route('login')}}"><i class="fa fa-user" aria-hidden="true"></i>Perfil</a>
             </span>
-                </div>
-
-                <div class="col-sm-3">
-                    <h4 class="title">Terminos</h4>
-                    <span class="acount-icon"><a href="{{route('terminos')}}">Terminos y Condiciones</a></span>
-                    <span class="acount-icon"><a href="{{route('politica')}}">Politica y tratamiento de datos</a></span>
-
-                </div>
-
-                <div class="col-sm-3">
-
-                </div>
             </div>
-            <div class="row">
-                <hr>
-                <div class="row text-center"><a href="https://www.codwelt.com" style="color: #fff;">Copyright © 2019 Codwelt.</a></div>
+
+            <div class="col-sm-3">
+                <h4 class="title">Terminos</h4>
+                <span class="acount-icon"><a href="{{route('terminos')}}">Terminos y Condiciones</a></span>
+                <span class="acount-icon"><a href="{{route('politica')}}">Politica y tratamiento de datos</a></span>
+
+            </div>
+
+            <div class="col-sm-3">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <hr style="color: #fff;">
+                <a class="text-center" href="https://www.codwelt.com" style="color: #fff;">Copyright © 2019
+                        Codwelt.</a>
             </div>
 
         </div>
-    </footer>
+
+    </div>
+</footer>
 </body>
 </html>
