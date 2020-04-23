4cb965e8d977f55dd97313418315b4b8dedf479d
FuriosoJack <iam@furiosojack.com>
2020-04-23 15:59:52 -0500

    SE añade envio de email soporte

--- resources/views/usuario/soporte.blade.php
+++ resources/views/usuario/soporte.blade.php
@@ -10,13 +10,13 @@
                     <h5 class="card-header">Soporte</h5>
                     <div class="card-body">
                         <p>
-                            Hola, necesitas ayuda en algo. Escribenos para poder ayudarte 😬<i class="em em-face_with_hand_over_mouth" aria-role="presentation" ></i>
+                            Hola, necesitas ayuda en algo. Escribenos para poder ayudarte😬<i class="em em-face_with_hand_over_mouth" aria-role="presentation" ></i>
                         </p>
                         <form method="post" action="{{route('soporte.store')}}">
                             @csrf
                             <div class="form-group">
-                                <label for="exampleFormControlTextarea1">Mensaje</label>
                                 <textarea class="form-control" name="mensaje" id="exampleFormControlTextarea1" rows="3" required></textarea>
+                                <small id="emailHelp" class="form-text text-muted">Descripcion del problema</small>
                             </div>
                             <button type="submit" class="btn btn-primary">Enviar </button>
                         </form>
